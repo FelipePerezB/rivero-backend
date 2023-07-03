c31fbf3730e0455abf7fd273d2008f457bb4b181
= <=>
2023-07-03 19:36:29 -0400

    0.0.22

--- api/src/docs/update-doc.input.ts
+++
@@ -1,8 +0,0 @@
-import { CreateDocInput } from './create-doc.input';
-import { InputType, Field, Int, PartialType } from '@nestjs/graphql';
-
-@InputType()
-export class UpdateDocInput extends PartialType(CreateDocInput) {
-  @Field(() => Int)
-  id: number;
-}
--- api/src/school-entities/grades/dto/update-grade.input.ts
+++
@@ -1,8 +0,0 @@
-import { CreateGradeInput } from './create-grade.input';
-import { InputType, Field, Int, PartialType } from '@nestjs/graphql';
-
-@InputType()
-export class UpdateGradeInput extends PartialType(CreateGradeInput) {
-  @Field(() => Int)
-  id: number;
-}
