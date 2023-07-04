5b0dd4d37bc7c5291a297e588a4252db5767613f
= <=>
2023-07-03 21:32:19 -0400

    0.0.30

--- src/app.module.ts
+++ src/app.module.ts
@@ -20,7 +20,7 @@ import { AuthModule } from './auth/auth.module';
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
-      autoSchemaFile: join(process.cwd(), '/tmp/schema.gql'),
+      autoSchemaFile: '/tmp/schema.gql',
       // autoSchemaFile:  true,
       playground: false,
       introspection: true,
