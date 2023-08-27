e36057b6f0f3f24155d700f54544b2afd962822e
= <=>
2023-08-27 00:33:33 -0400

    bug fix

--- src/subjects/subjects.service.ts
+++ src/subjects/subjects.service.ts
@@ -64,6 +64,7 @@ export class SubjectsService {
                   select: {
                     title: true,
                     id: true,
+                    externalId: true,
                   },
                 },
               },
