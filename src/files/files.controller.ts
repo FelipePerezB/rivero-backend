1f2ee170b0d807cbabe4f6e8758e46f5cc03388e
= <=>
2023-10-02 16:31:03 -0300

    files controller

---
+++ src/files/files.controller.ts
@@ -0,0 +1,54 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Delete,
+  HttpCode,
+  HttpStatus,
+  ParseIntPipe,
+  Put,
+} from '@nestjs/common';
+import { FilesService } from './files.service';
+import { Prisma, File } from '@prisma/client';
+
+@Controller('files')
+export class FilesController {
+  constructor(private readonly filesService: FilesService) {}
+
+  @HttpCode(HttpStatus.OK)
+  @Get()
+  findAll(): Promise<File[]> {
+    return this.filesService.findAll({});
+  }
+
+  @HttpCode(HttpStatus.CREATED)
+  @Post()
+  async signupUser(@Body() fileData: Prisma.FileCreateInput): Promise<File> {
+    return this.filesService.create(fileData);
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Get(':externalId')
+  findOne(
+    @Param('externalId', ParseIntPipe) externalId: string,
+  ): Promise<File> {
+    return this.filesService.findOne({ externalId });
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Put(':id')
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() data: Prisma.FileUpdateInput,
+  ): Promise<File> {
+    return this.filesService.update({ where: { id }, data });
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Delete(':id')
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.filesService.remove({ id });
+  }
+}
